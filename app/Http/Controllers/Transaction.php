1aa4b28e8a3512aaeb590e7fe3c61694629b0bfc
Olawuyi Ayansola <codedev85@gmail.com>
2022-06-22 23:37:20 +0100

    transaction filter

--- app/Http/Controllers/Transaction.php
+++ app/Http/Controllers/Transaction.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Service;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\Transaction as Tranx;
 use RealRashid\SweetAlert\Facades\Alert;
@@ -10,9 +12,157 @@ class Transaction extends Controller
 {
     public function allTransactions()
     {
-        $transactions  = Tranx::with('schedule','user')->orderBy('created_at','desc')->Simplepaginate(30);
 
-        return view('admin.transactions.transaction', compact('transactions'));
+        $services = Service::all();
+
+        $serviceObject = new \stdClass();
+
+        foreach($services as $index => $service) {
+            if($service->name != 'Flight Booking' && $service->name !='Hotel Booking' )
+            {
+                $serviceObject->$index['id'] = $service->id;
+                $serviceObject->$index['service'] = $service->name;
+            }
+        }
+
+        $serviceData  = $serviceObject;
+
+
+
+        $transactions  = Tranx::with('schedule','user')
+                            ->orderBy('created_at','desc')
+                            ->Simplepaginate(30);
+        if(!is_null(request()->start_date))
+        {
+            $transactions  = Tranx::with('schedule','user')
+                ->whereDate('created_at','>=',request()->start_date)
+                ->orderBy('created_at','desc')
+                ->Simplepaginate(30);
+
+        }
+
+        if(!is_null(request()->end_date))
+        {
+            $transactions  = Tranx::with('schedule','user')
+                ->whereDate('created_at','<=',request()->end_date)
+                ->orderBy('created_at','desc')
+                ->Simplepaginate(30);
+
+        }
+
+
+        if(!is_null(request()->end_date) && !is_null(request()->start_date))
+        {
+            $transactions  = Tranx::with('schedule','user')
+                ->whereDate('created_at','>=',request()->start_date)
+                ->whereDate('created_at','<=',request()->end_date)
+                ->orderBy('created_at','desc')
+                ->Simplepaginate(30);
+
+        }
+
+        if(!is_null(request()->service_type))
+        {
+            $transactions  = Tranx::with('schedule','user')
+                ->where('service_id',request()->service_type)
+                ->orderBy('created_at','desc')
+                ->Simplepaginate(30);
+        }
+
+
+        if(!is_null(request()->service_type)  && !is_null(request()->start_date))
+        {
+            $transactions  = Tranx::with('schedule','user')
+                ->whereDate('created_at','>=',request()->start_date)
+                ->where('service_id',request()->service_type)
+                ->orderBy('created_at','desc')
+                ->Simplepaginate(30);
+        }
+
+        if(!is_null(request()->service_type)  && !is_null(request()->end_date))
+        {
+            $transactions  = Tranx::with('schedule','user')
+                ->whereDate('created_at','<=',request()->end_date)
+                ->where('service_id',request()->service_type)
+                ->orderBy('created_at','desc')
+                ->Simplepaginate(30);
+        }
+
+        if(!is_null(request()->service_type)  && !is_null(request()->start_date)  && !is_null(request()->end_date))
+        {
+            $transactions  = Tranx::with('schedule','user')
+                ->whereDate('created_at','>=',request()->start_date)
+                ->whereDate('created_at','<=',request()->end_date)
+                ->where('service_id',request()->service_type)
+                ->orderBy('created_at','desc')
+                ->Simplepaginate(30);
+        }
+
+        if(!is_null(request()->transaction_status))
+        {
+            $transactions  = Tranx::with('schedule','user')
+                ->where('status',request()->transaction_status)
+                ->orderBy('created_at','desc')
+                ->Simplepaginate(30);
+        }
+
+
+        if(!is_null(request()->transaction_status)  && !is_null(request()->start_date) &&  !is_null(request()->end_date))
+        {
+            $transactions  = Tranx::with('schedule','user')
+                ->where('status',request()->transaction_status)
+                ->whereDate('created_at','>=',request()->start_date)
+                ->whereDate('created_at','<=',request()->end_date)
+                ->orderBy('created_at','desc')
+                ->Simplepaginate(30);
+        }
+
+        if(!is_null(request()->transaction_status)  && !is_null(request()->service_type) &&
+            !is_null(request()->start_date) &&  !is_null(request()->end_date))
+        {
+            $transactions  = Tranx::with('schedule','user')
+                ->where('status',request()->transaction_status)
+                ->whereDate('created_at','>=',request()->start_date)
+                ->whereDate('created_at','<=',request()->end_date)
+                ->where('service_id',request()->service_type)
+                ->orderBy('created_at','desc')
+                ->Simplepaginate(30);
+        }
+
+        if(!is_null(request()->payment_type))
+        {
+            $transactions  = Tranx::with('schedule','user')
+                ->where('transaction_type',request()->payment_type)
+                ->orderBy('created_at','desc')
+                ->Simplepaginate(30);
+        }
+
+        if(!is_null(request()->reference))
+        {
+            $transactions  = Tranx::with('schedule','user')
+                ->where('reference',request()->reference)
+                ->orderBy('created_at','desc')
+                ->Simplepaginate(30);
+        }
+
+        if(!is_null(request()->passenger_email))
+        {
+            $user = User::where('email', request()->passenger_email)->first();
+            if($user){
+                $transactions  = Tranx::with('schedule','user')
+                    ->where('user_id',$user->id)
+                    ->orderBy('created_at','desc')
+                    ->Simplepaginate(30);
+            }else{
+                Alert::error('Error', 'User with the email not found');
+//                return back();
+            }
+
+
+        }
+
+
+        return view('admin.transactions.transaction', compact('transactions','serviceData'));
     }
 
 
--- resources/views/admin/transactions/transaction.blade.php
+++ resources/views/admin/transactions/transaction.blade.php
@@ -32,7 +32,8 @@
             </div>
             <div class="card col-md-12 col-lg-12 col-sm-12 col-xl-12 col-xs-12">
                 <div class="card-body">
-                   <form>
+                   <form action="{{url('admin/transactions')}}">
+                       @csrf
                        <div class="grid">
                            <div>
                                <div class="form-group">
@@ -41,7 +42,7 @@
                                </div>
                                <div class="form-group">
                                    <label>End Date</label>
-                                   <input type="date" class="form-control" name="start_date"/>
+                                   <input type="date" class="form-control" name="end_date"/>
                                </div>
                            </div>
 
@@ -50,19 +51,18 @@
                                    <label>Service Type</label>
                                    <select class="form-control" name="service_type">
                                        <option value="">Select Services</option>
-                                       <option value="">Bus Booking</option>
-                                       <option value="successful">Flight Booking</option>
-                                       <option value="cancelled">Ferry Booking</option>
+                                       @foreach($serviceData as $service)
+                                       <option value="{{$service['id']}}">{{$service['service']}}</option>
+                                       @endforeach
                                    </select>
                                </div>
                                <div class="form-group">
                                    <label>Transaction Status</label>
                                    <select class="form-control" name="transaction_status">
                                        <option value="">Select Status</option>
-                                       <option value="successful">Successful</option>
-                                       <option value="cancelled">Cancelled</option>
-                                       <option value="failed">Failed</option>
-                                       <option value="fraud-detected">Likely Fraud</option>
+                                       <option value="Successful">Successful</option>
+                                       <option value="Pending">Pending</option>
+                                       <option value="Likely Fraud">Likely Fraud</option>
                                    </select>
                                </div>
                            </div>
@@ -73,18 +73,22 @@
                                </div>
                                <div class="form-group">
                                    <label>Transaction Reference</label>
-                                   <input type="text" class="form-control" name="start_date"/>
+                                   <input type="text" class="form-control" name="reference"/>
                                </div>
                            </div>
                            <div>
                                <div class="form-group">
-                                   <label>Flutterwave Trasanction Reference</label>
-                                   <input type="tx_reference" class="form-control" name="passenger_email"/>
+                                   <label>Payment Type</label>
+                                   <select class="form-control" name="payment_type">
+                                       <option value="">Select Status</option>
+                                       <option value="cash payment">Cash Payment</option>
+                                       <option value="online">Card Payment</option>
+                                   </select>
                                </div>
-                               <div class="form-group">
-                                   <label>Reference</label>
-                                   <input type="text" class="form-control" name="start_date"/>
-                               </div>
+{{--                               <div class="form-group">--}}
+{{--                                   <label>Reference</label>--}}
+{{--                                   <input type="text" class="form-control" name="start_dateeee"/>--}}
+{{--                               </div>--}}
                            </div>
                            <div class="search_btn">
                                <button class="btn btn-success">Search Filter</button>
@@ -107,7 +111,7 @@
                         <th scope="col">Amount</th>
                         <th scope="col">Service Type</th>
                         <th scope="col">Reference</th>
-                        <th scope="col">Flutterwave Reference</th>
+{{--                        <th scope="col">Flutterwave Reference</th>--}}
                         <th scope="col">Purchased By</th>
                         <th scope="col">Status</th>
                         <th scope="col">Date</th>
@@ -121,7 +125,7 @@
                                     <td>&#8358; {{number_format($transaction->amount)}}</td>
                                     <td>{{$transaction->service->name}}</td>
                                     <td>{{$transaction->reference}}</td>
-                                    <td>{{$transaction->trx_ref}}</td>
+{{--                                    <td>{{$transaction->trx_ref}}</td>--}}
                                     <td>{{$transaction->user->full_name}}</td>
                                     <td>{{$transaction->status}}</td>
                                     <td>{{$transaction->created_at->diffforhumans()}}</td>
