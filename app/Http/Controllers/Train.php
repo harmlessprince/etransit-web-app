dbc493b9a1ab48314b492a46b7b1fa84b14760f4
Olawuyi Ayansola <codedev85@gmail.com>
2022-07-12 11:03:55 +0100

    fixing bugs in train shcedule

--- app/Http/Controllers/Train.php
+++ app/Http/Controllers/Train.php
@@ -499,7 +499,7 @@ class Train extends Controller
         $routeFare = ScheduleRoute::where('train_schedule_id',$train_schedule_id)->with(['routeFare' => function($query){
             $query->with('terminal','destination_terminal','seatClass')->get();
         }])->get();
-        dd($routeFare->routeFare[0]);
+        dd($routeFare[0]->routeFare);
 
 
         return view('pages.train.seat-picker', compact('trainSeatsPicker','schedule_id','routeFare','tranId','trip_type','train_schedule_id'));
