587180105fb2a80d8b136ae97724735675e9562d
Olawuyi Ayansola <codedev85@gmail.com>
2022-07-12 10:28:31 +0100

    fixing bugs in train shcedule

--- app/Http/Controllers/Api/Train.php
+++ app/Http/Controllers/Api/Train.php
@@ -164,7 +164,9 @@ class Train extends Controller
 
     public function routeSelector($train_schedule_id)
     {
-        $fetchRoutes = ScheduleRoute::where('train_schedule_id', $train_schedule_id)->with('routeFare')->get();
+        $fetchRoutes = ScheduleRoute::where('train_schedule_id', $train_schedule_id)->with(['routeFare' => function($query){
+            $query->with('city', 'destination','terminal','destination_terminal','seatClass')->get();
+        }])->get();
 //        trainRoutes
 
         return response()->json(['success' => true , 'data' => compact('fetchRoutes')]);
