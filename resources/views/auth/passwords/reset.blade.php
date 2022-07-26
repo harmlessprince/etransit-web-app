a3918810a2f1048d05a980372dada2c46322e3e4
Samuel Egemba <egembasam@gmail.com>
2022-07-26 11:29:44 +0100

    illustration assets

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -99,7 +99,7 @@
     <link rel="stylesheet" href="{{asset('login-assets/csss/Ultimate-Testimonial-Slider-BS5.css')}}">
     <style>
         body {
-            background-image: url('{{ asset('login-assets/img/Rectangle%203.png')}}') ;
+            background-image: url('../login-assets/img/SignIn.png');
             background-repeat: no-repeat;
             background-attachment: fixed;
             background-size: cover;
