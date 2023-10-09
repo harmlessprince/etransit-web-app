4c31e73395335163b2b61bf1c476b6aa3fcc05f7
Donald Blessing <donaldblessing9@gmail.com>
2023-10-09 14:45:19 +0100

    fixed some errors

---
+++ database/seeders/ScheduleData.php
@@ -0,0 +1,32 @@
+<?php
+
+
+declare(strict_types=1);
+
+
+namespace Database\Seeders;
+
+class ScheduleData
+{
+
+    public $operator;
+    public $terminal;
+    public $number_plate;
+    public $pickup;
+    public $seats;
+    public $destination;
+    public $adult_price;
+    public $child_price;
+    public $departure_date;
+    public $departure_time;
+    public $return_trip;
+    public $return_date;
+    public $return_time;
+    public $terminal_address;
+    public $operator_address;
+    public $operator_phone;
+    public $bus_model;
+    public $bus_type;
+
+
+}
--- database/seeders/ScheduleSeeder.php
+++ database/seeders/ScheduleSeeder.php
@@ -21,27 +21,4 @@ class ScheduleSeeder extends Seeder
     }
 }
 
-class ScheduleData
-{
 
-    public $operator;
-    public $terminal;
-    public $number_plate;
-    public $pickup;
-    public $seats;
-    public $destination;
-    public $adult_price;
-    public $child_price;
-    public $departure_date;
-    public $departure_time;
-    public $return_trip;
-    public $return_date;
-    public $return_time;
-    public $terminal_address;
-    public $operator_address;
-    public $operator_phone;
-    public $bus_model;
-    public $bus_type;
-
-
-}
