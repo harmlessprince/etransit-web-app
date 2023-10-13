3b6bb5735d104ca2269b6bfe8101f6f8db65d620
Donald Blessing <donaldblessing9@gmail.com>
2023-10-13 17:59:48 +0100

    Autocomplete address fixed

--- resources/views/pages/car-hire/pick-up-date.blade.php
+++ resources/views/pages/car-hire/pick-up-date.blade.php
@@ -122,7 +122,7 @@
         </div>
         <div class="date_picker_box">
             <div class="date_picker_form">
-                <div>
+                <div class="pac-card" id="pac-card">
                     <form method="POST" action="{{url('plan/'.$findPaymentOption->id)}}">
                         @csrf
 
@@ -145,11 +145,52 @@
                         </div>
                         <br>
                         <div class="form-group">
-                            <label for="pickup_address">Pickup Address</label>
-                            <input type="text" autocomplete name="pickup_address" id="pickup_address"
-                                   value="{{ old('pickup_address') }}"
-                                   class="form-control" required/>
-                            <div id="autocompleteResults"></div>
+                            <div class="pac-card" id="pac-card">
+                                <div style="display: none;">
+                                    <div id="type-selector" class="pac-controls">
+                                        <input
+                                            type="radio"
+                                            name="type"
+                                            id="changetype-all"
+                                            checked="checked"
+                                        />
+                                        <label for="changetype-all">All</label>
+
+                                        <input type="radio" name="type" id="changetype-establishment"/>
+                                        <label for="changetype-establishment">establishment</label>
+
+                                        <input type="radio" name="type" id="changetype-address"/>
+                                        <label for="changetype-address">address</label>
+
+                                        <input type="radio" name="type" id="changetype-geocode"/>
+                                        <label for="changetype-geocode">geocode</label>
+
+                                        <input type="radio" name="type" id="changetype-cities"/>
+                                        <label for="changetype-cities">(cities)</label>
+
+                                        <input type="radio" name="type" id="changetype-regions"/>
+                                        <label for="changetype-regions">(regions)</label>
+                                    </div>
+                                    <br/>
+                                    <div id="strict-bounds-selector" class="pac-controls">
+                                        <input type="checkbox" id="use-location-bias" value="" checked/>
+                                        <label for="use-location-bias">Bias to map viewport</label>
+
+                                        <input type="checkbox" id="use-strict-bounds" value=""/>
+                                        <label for="use-strict-bounds">Strict bounds</label>
+                                    </div>
+                                </div>
+                                <div id="pac-container">
+                                    <label for="pickup_address">Pickup Address</label>
+                                    <input id="pickup_address" name="pickup_address" type="text" class="form-control"
+                                           placeholder="Enter a pickup address" value="{{ old('pickup_address') }}"/>
+                                </div>
+                            </div>
+                            <div id="map"></div>
+                            <div id="infowindow-content">
+                                <span id="place-name" class="title"></span><br/>
+                                <span id="place-address"></span>
+                            </div>
                         </div>
                         <br>
 
@@ -244,31 +285,123 @@
     </div>
 
     <script
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCl3OQUTqcOTP55fo2Z089F6IThkJIyako&libraries=places&callback=initMap"
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCl3OQUTqcOTP55fo2Z089F6IThkJIyako&callback=initMap&libraries=places&v=weekly"
         defer></script>
 
     <script type="text/javascript">
 
-        // Initialize the PlacesService
-        const placesService = new google.maps.places.PlacesService(document.createElement('div'));
 
-        // Initialize the autocomplete object
-        const autocomplete = new google.maps.places.Autocomplete(
-            document.getElementById('pickup_address')
-        );
+        // This example requires the Places library. Include the libraries=places
+        // parameter when you first load the API. For example:
+        // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+        function initMap() {
+            const map = new google.maps.Map(document.getElementById("map"), {
+                center: {lat: 40.749933, lng: -73.98633},
+                zoom: 13,
+                mapTypeControl: false,
+            });
+            const card = document.getElementById("pac-card");
+            const input = document.getElementById("pickup_address");
+            const biasInputElement = document.getElementById("use-location-bias");
+            const strictBoundsInputElement = document.getElementById("use-strict-bounds");
+            const options = {
+                fields: ["formatted_address", "geometry", "name"],
+                strictBounds: false,
+            };
 
-        console.log(autocomplete);
+            map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
 
+            const autocomplete = new google.maps.places.Autocomplete(input, options);
 
-        // Event listener for place selection
-        autocomplete.addListener('place_changed', function () {
-            const place = autocomplete.getPlace();
-            const address = place.formatted_address;
-            // Do something with the selected address
-            console.log('Selected address: ' + address);
-        });
+            // Bind the map's bounds (viewport) property to the autocomplete object,
+            // so that the autocomplete requests use the current map bounds for the
+            // bounds option in the request.
+            autocomplete.bindTo("bounds", map);
 
-        // You can customize the autocomplete options
-        autocomplete.setTypes(['geocode']);
+            const infowindow = new google.maps.InfoWindow();
+            const infowindowContent = document.getElementById("infowindow-content");
+
+            infowindow.setContent(infowindowContent);
+
+            const marker = new google.maps.Marker({
+                map,
+                anchorPoint: new google.maps.Point(0, -29),
+            });
+
+            autocomplete.addListener("place_changed", () => {
+                infowindow.close();
+                marker.setVisible(false);
+
+                const place = autocomplete.getPlace();
+
+                if (!place.geometry || !place.geometry.location) {
+                    // User entered the name of a Place that was not suggested and
+                    // pressed the Enter key, or the Place Details request failed.
+                    window.alert("No details available for input: '" + place.name + "'");
+                    return;
+                }
+
+                // If the place has a geometry, then present it on a map.
+                if (place.geometry.viewport) {
+                    map.fitBounds(place.geometry.viewport);
+                } else {
+                    map.setCenter(place.geometry.location);
+                    map.setZoom(17);
+                }
+
+                marker.setPosition(place.geometry.location);
+                marker.setVisible(true);
+                infowindowContent.children["place-name"].textContent = place.name;
+                infowindowContent.children["place-address"].textContent =
+                    place.formatted_address;
+                infowindow.open(map, marker);
+            });
+
+            // Sets a listener on a radio button to change the filter type on Places
+            // Autocomplete.
+            function setupClickListener(id, types) {
+                const radioButton = document.getElementById(id);
+
+                radioButton.addEventListener("click", () => {
+                    autocomplete.setTypes(types);
+                    input.value = "";
+                });
+            }
+
+            setupClickListener("changetype-all", []);
+            setupClickListener("changetype-address", ["address"]);
+            setupClickListener("changetype-establishment", ["establishment"]);
+            setupClickListener("changetype-geocode", ["geocode"]);
+            setupClickListener("changetype-cities", ["(cities)"]);
+            setupClickListener("changetype-regions", ["(regions)"]);
+            biasInputElement.addEventListener("change", () => {
+                if (biasInputElement.checked) {
+                    autocomplete.bindTo("bounds", map);
+                } else {
+                    // User wants to turn off location bias, so three things need to happen:
+                    // 1. Unbind from map
+                    // 2. Reset the bounds to whole world
+                    // 3. Uncheck the strict bounds checkbox UI (which also disables strict bounds)
+                    autocomplete.unbind("bounds");
+                    autocomplete.setBounds({east: 180, west: -180, north: 90, south: -90});
+                    strictBoundsInputElement.checked = biasInputElement.checked;
+                }
+
+                input.value = "";
+            });
+            strictBoundsInputElement.addEventListener("change", () => {
+                autocomplete.setOptions({
+                    strictBounds: strictBoundsInputElement.checked,
+                });
+                if (strictBoundsInputElement.checked) {
+                    biasInputElement.checked = strictBoundsInputElement.checked;
+                    autocomplete.bindTo("bounds", map);
+                }
+
+                input.value = "";
+            });
+        }
+
+        window.initMap = initMap;
     </script>
 @endsection
